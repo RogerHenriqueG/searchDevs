e0296ac77f97d51bef4b2018940d1c36e9b96b6f
rogerhenriqueg <roger.henrique.rh14@gmail.com>
2021-12-07 20:36:20 -0300

    links do meu perfil

--- resources/views/template/home.blade.php
+++ resources/views/template/home.blade.php
@@ -295,10 +295,9 @@
                         <h3>About Moderna</h3>
                         <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
                         <div class="social-links mt-3">
-                            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                            <a href="https://www.facebook.com/roger.henrique.39545" class="facebook"><i class="bx bxl-facebook"></i></a>
+                            <a href="https://www.instagram.com/by.roger_henri/" class="instagram"><i class="bx bxl-instagram"></i></a>
+                            <a href="https://www.linkedin.com/in/roger-henrique-72524b197/" class="linkedin"><i class="bx bxl-linkedin"></i></a>
                         </div>
                     </div>
 
